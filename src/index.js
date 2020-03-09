bbfa8eb5cf63e237f6ad018383ab42cdd0277838
Wad Werton <Wad Werton>
2020-03-09 20:25:33 +0300

    feat: add task brackets(3 tests fail)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracket = bracketsConfig.map(elem => elem.join('')).join('');
+  let n = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === bracket[0] || str[i] === bracket[2] || str[i] === bracket[4]) n++;
+    if (str[i] === bracket[1] || str[i] === bracket[3] || str[i] === bracket[5]) n--;
+    if (str[i] === bracket[0] && (str[i] !== '|' && str[i+1] !== '|') && (str[i+1] === bracket[3] || str[i+1] === bracket[5])) return false;
+    if (n < 0) return false;
+  }
+  return n === 0; 
 }
